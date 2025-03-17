dbf4e57466865abb31e55d227b4f99c2e840f99c
AloneDineshEver <108724696+Dinesh89-D@users.noreply.github.com>
2025-03-17 07:33:45 +0530

    add routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import Home from './Pages/Home'
+import About from './Pages/About'
+import SignIn from './Pages/SignIn'
+import SignUp from './Pages/SignUp'
+import Profile from './Pages/Profile'
 
 const App = () => {
   return (
-    <h1 className='text-red-500'>
-      APP12
-    </h1>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/about" element={<About/>}/>
+          <Route path="/signin" element={<SignIn/>}/>
+          <Route path="/signup" element={<SignUp/>}/>
+          <Route path="/profile" element={<Profile/>}/>  
+        </Routes>
+      </BrowserRouter>
+    </>
   )
 }
 
